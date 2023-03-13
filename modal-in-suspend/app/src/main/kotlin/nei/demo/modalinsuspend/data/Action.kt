6e67665398cc1de30e9838d73e0cbfa5ab7d6e61
Esneider Jaimes Jaimes <62038623+esneiderfjaimes@users.noreply.github.com>
2023-03-13 10:41:50 -0500

    remove demos in master

--- modal-in-suspend/app/src/main/kotlin/nei/demo/modalinsuspend/data/Action.kt
+++
@@ -1,9 +0,0 @@
-package nei.demo.modalinsuspend.data
-
-data class Action(val label: String, val type: String)
-
-val actions = listOf(
-    Action("Delete photo", "delete"),
-    Action("Take photo", "take"),
-    Action("Choose photo", "choose")
-)
